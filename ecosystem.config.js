37480bc5ea235b7c5d8435227daf5963fe4dbd9b
friedpies <kenmarut@gmail.com>
2019-06-13 14:20:30 -0500

    modify pm2 file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,10 +6,10 @@ module.exports = {
     deploy: {
         production: {
             user: 'ubuntu',
-            host: 'ec2-3-88-248-205.compute-1.amazonaws.com',
-            key: '~/.ssh/fec.pem',
+            host: 'ec2-18-217-82-96.us-east-2.compute.amazonaws.com',
+            key: '~/.ssh/sdc-carousel.pem',
             ref: 'origin/master',
-            repo: 'git@github.com:nike-hratx41-fec/carousel.git',
+            repo: 'git@github.com:sole-hr/carousel-ken.git',
             path: '/home/ubuntu/carousel',
             'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
         }
