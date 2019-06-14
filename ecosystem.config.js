6dd33441074d954ff8e839c73a51def268a9025f
friedpies <kenmarut@gmail.com>
2019-06-14 14:56:00 -0500

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,19 @@
 module.exports = {
-    apps: [{
-        name: 'carousel',
-        script: './server/index.js',
-    }],
-    deploy: {
-        production: {
-            user: 'ubuntu',
-            host: 'ec2-18-223-132-37.us-east-2.compute.amazonaws.com',
-            key: '~/.ssh/sdc-carousel.pem',
-            ref: 'origin/master',
-            repo: 'git@github.com:sole-hr/carousel-ken.git',
-            path: '/home/ubuntu/carousel/source',
-            'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
-        }
+  apps: [
+    {
+      name: "carousel",
+      script: "./server/index.js"
     }
-}
+  ],
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: "ec2-18-217-227-111.us-east-2.compute.amazonaws.com",
+      key: "~/.ssh/sdc-carousel.pem",
+      ref: "origin/master",
+      repo: "git@github.com:sole-hr/carousel-ken.git",
+      path: "/home/ubuntu/carousel/",
+      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js"
+    }
+  }
+};
