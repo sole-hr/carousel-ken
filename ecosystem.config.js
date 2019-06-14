7c5c135881f5809a66d079fd6d5b5e68b25037fe
friedpies <kenmarut@gmail.com>
2019-06-14 11:13:58 -0500

    trying on new instance

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,11 +6,11 @@ module.exports = {
     deploy: {
         production: {
             user: 'ubuntu',
-            host: 'ec2-18-217-82-96.us-east-2.compute.amazonaws.com',
+            host: 'ec2-18-223-132-37.us-east-2.compute.amazonaws.com',
             key: '~/.ssh/sdc-carousel.pem',
             ref: 'origin/master',
             repo: 'git@github.com:sole-hr/carousel-ken.git',
-            path: '~/carousel',
+            path: '/home/ubuntu/carousel',
             'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
         }
     }
